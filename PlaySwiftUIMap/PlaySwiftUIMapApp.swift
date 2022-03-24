2a3d9e9a50d27f85f9a4aef3afc40f09de1cef4c
taise-hikawa <cmmkarubi@gmail.com>
2022-03-24 23:18:58 +0900

    Initial Commit

---
+++ PlaySwiftUIMap/PlaySwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySwiftUIMapApp.swift
+//  PlaySwiftUIMap
+//
+//  Created by 樋川大聖 on 2022/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaySwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
